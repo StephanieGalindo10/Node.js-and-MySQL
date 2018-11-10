bc98bb60baa3072ce6b91985b27961cf42bee671
test <test@gmail.com>
2018-11-09 18:12:59 -0800

    added  final quanity redo code that was lost

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,7 +30,7 @@ var schema = {
   function showInventory() {
     connection.query('SELECT ItemID, ProductName, Price FROM products', function(err, rows, fields) {
       if (err) throw err;
-      
+
       console.log('Available products:');
     for(var i = 0; i < rows.length; i++) {
       console.log('Item ID: ' + rows[i].ItemID + '   Product Name: ' + rows[i].ProductName + '   Price: $' + rows[i].Price);
@@ -48,3 +48,39 @@ function runPrompt() {
       processOrder(orderedProductID, orderQuantity);
     });
   }
+
+  function processOrder(id, quantity) {
+    connection.query('SELECT StockQuantity FROM products WHERE ItemID = ?', [id], function(err, rows, fields) {
+      if(err) throw err;
+  
+      if(JSON.parse(rows[0].StockQuantity) >= quantity) {
+        var adjQuantity = rows[0].StockQuantity - quantity;
+        getPrice(id, quantity);
+        updateStock(adjQuantity, id);
+      } else {
+        console.log('Oops,There is not enough stock to fulfill your request, please try again');
+        connection.end();
+      }
+    });
+  }
+  
+
+
+  function getPrice(id, quantity) {
+    connection.query('SELECT Price FROM products WHERE ItemID = ?', [id], function(err, rows, fields) {
+      if(err) throw err;
+      var orderPrice = JSON.parse(rows[0].Price) * quantity;
+      console.log('The total order cost is: $' + orderPrice);
+    });
+  }
+  
+  function updateStock(adjQuantity, id) {
+    connection.query('UPDATE products SET stockQuantity = ? WHERE ItemID = ?', [adjQuantity, id], function(err, rows, fields) {
+      if(err) throw err;
+      console.log('Inventory has been updated');
+      connection.end();
+    });
+  }
+  
+
+  showInventory();
